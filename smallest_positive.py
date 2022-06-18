cbef006f6782f1c6c051a5f3c2165422db799115
Anfal <muhammadanfal0@gmail.com>
2022-06-18 12:26:20 -0700

    WIP

---
+++ smallest_positive.py
@@ -0,0 +1,28 @@
+def smallest_positive(in_list):
+    # TODO: Define a control structure that finds the smallest positive
+    # number in in_list and returns the correct smallest number.
+    smallest_number = None
+
+    for number in in_list:
+        if number > 0:
+            if smallest_number is None:
+                smallest_number = number
+            elif number < smallest_number:
+                smallest_number = number
+
+    return smallest_number
+
+
+# Test cases
+
+print(smallest_positive([4, -6, 7, 2, -4, 10]))
+# Correct output: 2
+
+print(smallest_positive([.2, 5, 3, -.1, 7, 7, 6]))
+# Correct output: 0.2
+
+print(smallest_positive([-6, -9, -7]))
+# Correct output: None
+
+print(smallest_positive([]))
+# Correct output: None
