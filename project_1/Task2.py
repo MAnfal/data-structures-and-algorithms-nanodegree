8fe9b231ee50031fe5dbf116f5d0f1c5621bbcd8
Anfal <muhammadanfal0@gmail.com>
2022-06-25 17:34:41 -0700

    Fix the code for task 2.

--- project_1/Task2.py
+++ project_1/Task2.py
@@ -20,12 +20,46 @@ Print a message:
 September 2016.".
 """
 
-longest_call_record = None
+# Assumptions.
+# 1. The call we will process here will be in September 2016. Everything else will be auto-ignored.
+# 2. We will look at both calling and called to numbers, build a hashmap using dicitonary and process it find out max time.
+
+
+number_to_time_spent_map = {}
+longest_call_record = {
+    'number': None,
+    'seconds': None
+}
+
+
+def is_call_in_sep_2016(date_time_string):
+    return '9-2016' in date_time_string
+
+
+def add_record_in_dict(number, time):
+    if number in number_to_time_spent_map:
+        number_to_time_spent_map[number] += time
+    else:
+        number_to_time_spent_map[number] = time
+
 
 for call_record in calls:
-    call_record[3] = int(call_record[3])
+    calling_number = call_record[0]
+    called_to_number = call_record[1]
+    date_time = call_record[2]
 
-    if longest_call_record is None or longest_call_record[3] < call_record[3]:
-        longest_call_record = call_record
+    if is_call_in_sep_2016(date_time):
+        # we are going to perform a str -> int conversion so we should better be sure we need it.
+        # that's why calling it inside the if condition.
+        time_spent = int(call_record[3])
 
-print("{} spent the longest time, {} seconds, on the phone during {}".format(longest_call_record[1], longest_call_record[3], longest_call_record[2]))
+        add_record_in_dict(calling_number, time_spent)
+        add_record_in_dict(called_to_number, time_spent)
+
+for number, time_in_seconds in number_to_time_spent_map.items():
+    if longest_call_record['seconds'] is None or longest_call_record['seconds'] < time_in_seconds:
+        longest_call_record['number'] = number
+        longest_call_record['seconds'] = time_in_seconds
+
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(longest_call_record['number'], longest_call_record['seconds']))
--- project_1/practice.py
+++ project_1/practice.py
@@ -1,9 +1,8 @@
 """
 Read file into texts and calls.
-It's ok if you don't understand how to read files.
+It's ok if you don't understand how to read files
 """
 import csv
-
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -13,47 +12,54 @@ with open('calls.csv', 'r') as f:
     calls = list(reader)
 
 """
-TASK 4:
-The telephone company want to identify numbers that might be doing
-telephone marketing. Create a set of possible telemarketers:
-these are numbers that make outgoing calls but never send texts,
-receive texts or receive incoming calls.
-
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
 Print a message:
-"These numbers could be telemarketers: "
-<list of numbers>
-The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
 """
 
-# Assumptions
-# 1. Number is a telemarketer number so it'll start with 140, have no () and no spaces.
-# 2. It won't send/receive texts so we only have to look in the calls CSV.
-# 3. If in the calls CSV we found out that a certain number had a call made to but was telemarketer, remove that from our results.
+# Assumptions.
+# 1. The call we will process here will be in September 2016. Everything else will be auto-ignored.
+# 2. We will look at both calling and called to numbers, build a hashmap using dicitonary and process it find out max time.
 
 
-telemarketer_dictionary = {}
+number_to_time_spent_map = {}
+longest_call_record = {
+    'number': None,
+    'seconds': None
+}
 
 
-def is_telemarketer_number(phone_number):
-    return '(' not in phone_number and \
-           ')' not in phone_number and \
-           ' ' not in phone_number and \
-           phone_number[:3] == '140'
+def is_call_in_sep_2016(date_time_string):
+    return '9-2016' in date_time_string
+
+
+def add_record_in_dict(number, time):
+    if number in number_to_time_spent_map:
+        number_to_time_spent_map[number] += time
+    else:
+        number_to_time_spent_map[number] = time
 
 
 for call_record in calls:
     calling_number = call_record[0]
-
-    if is_telemarketer_number(calling_number) and calling_number not in telemarketer_dictionary:
-        telemarketer_dictionary[calling_number] = 1
-
-for call_record in calls:
     called_to_number = call_record[1]
+    date_time = call_record[2]
 
-    if is_telemarketer_number(called_to_number) and called_to_number in telemarketer_dictionary:
-        del telemarketer_dictionary[called_to_number]
+    if is_call_in_sep_2016(date_time):
+        # we are going to perform a str -> int conversion so we should better be sure we need it.
+        # that's why calling it inside the if condition.
+        time_spent = int(call_record[3])
 
-print('These numbers could be telemarketers: ')
+        add_record_in_dict(calling_number, time_spent)
+        add_record_in_dict(called_to_number, time_spent)
 
-for number in sorted(telemarketer_dictionary.keys()):
-    print(number)
+for number, time_in_seconds in number_to_time_spent_map.items():
+    if longest_call_record['seconds'] is None or longest_call_record['seconds'] < time_in_seconds:
+        longest_call_record['number'] = number
+        longest_call_record['seconds'] = time_in_seconds
+
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(longest_call_record['number'], longest_call_record['seconds']))
